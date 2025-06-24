afd6e96472de3ef3288e0fae25c425f7528594a8
Anna Santos <anna.clara16f@yahoo.com.br>
2025-06-24 14:33:29 -0300

    Ediçao de erros e alteração nas mensagens

--- script.js
+++ script.js
@@ -1,3 +1,130 @@
+// let jogador = "";
+// let pontuacao = {};
+// let jogadas = 0;
+
+// let vitorias = {
+//     X: 0,
+//     O: 0
+// };
+
+// let ultimoVencedor = "X"; 
+
+// function iniciarJogo() {
+//     jogadas = 0;
+//     jogador = ultimoVencedor; 
+//     document.querySelector("#jogadorDoTurno").textContent = jogador;
+//     document.getElementById("vencedor").innerText = "";
+
+//     pontuacao = {
+//         X: {
+//             linha1: 0,
+//             linha2: 0,
+//             linha3: 0,
+//             coluna1: 0,
+//             coluna2: 0,
+//             coluna3: 0,
+//             diagonal1: 0,
+//             diagonal2: 0
+//         },
+//         O: {
+//             linha1: 0,
+//             linha2: 0,
+//             linha3: 0,
+//             coluna1: 0,
+//             coluna2: 0,
+//             coluna3: 0,
+//             diagonal1: 0,
+//             diagonal2: 0
+//         }
+//     };
+
+//     let campos = document.querySelectorAll("div.campo");
+//     for (let campo of campos) {
+//         campo.textContent = "";
+//         campo.addEventListener("click", marcarCampo);
+//     }
+
+//     atualizarPlacar();
+// }
+
+// function removerEventosDosCampos() {
+//     let campos = document.querySelectorAll("div.campo");
+//     for (let campo of campos) {
+//         campo.removeEventListener("click", marcarCampo);
+//     }
+// }
+
+// function marcarCampo(event) {
+//     if (event.target.textContent === "") {
+//         event.target.textContent = jogador;  
+//         jogadas++;
+
+//         let classes = event.target.classList.value.split(" ");
+//         for (let classe of classes) {
+//             if (pontuacao[jogador][classe] !== undefined) {
+//                 pontuacao[jogador][classe]++;
+//             }
+//         }
+
+     
+//         document.querySelector("#jogadorDoTurno").textContent = jogador;
+
+//         if (conferirResultado(classes)) return;
+//         if (verificarEmpate()) return;
+
+//         trocarJogador();
+//     }
+// }
+
+// function trocarJogador() {
+//     jogador = jogador === "X" ? "O" : "X";
+// }
+
+// function conferirResultado(classes) {
+//     for (let classe of classes) {
+//         if (pontuacao[jogador][classe] === 3) {
+//             vitorias[jogador]++;
+//             ultimoVencedor = jogador;
+
+//             atualizarPlacar();
+//             document.getElementById('vencedor').innerText = "Jogador " + jogador + " venceu!!";
+
+//             removerEventosDosCampos();
+
+//             setTimeout(function () {
+//                 alert("Jogador " + jogador + " venceu!!");
+//             }, 50);
+
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// function verificarEmpate() {
+//     if (jogadas >= 9) {
+//         removerEventosDosCampos();
+//         document.getElementById('vencedor').innerText = "Deu velha!! Último a jogar: " + jogador;
+
+ 
+//         setTimeout(function () {
+//             alert("Deu velha!!");
+//         }, 50);
+
+//         return true;
+//     }
+//     return false;
+// }
+
+// function atualizarPlacar() {
+//     document.getElementById("vitoriasX").textContent = vitorias.X;
+//     document.getElementById("vitoriasO").textContent = vitorias.O;
+// }
+
+// document.querySelector("#reiniciar").addEventListener("click", iniciarJogo);
+
+// iniciarJogo();
+
 let jogador = "";
 let pontuacao = {};
 let jogadas = 0;
@@ -7,13 +134,13 @@ let vitorias = {
     O: 0
 };
 
-let ultimoVencedor = "X"; // Começa com X por padrão
+let ultimoVencedor = "X"; 
 
 function iniciarJogo() {
     jogadas = 0;
-    jogador = ultimoVencedor; // O último vencedor começa
+    jogador = ultimoVencedor; 
     document.querySelector("#jogadorDoTurno").textContent = jogador;
-    document.getElementById("vencedor").innerText = ""; 
+    document.getElementById("vencedor").innerText = "";
 
     pontuacao = {
         X: {
@@ -82,16 +209,22 @@ function conferirResultado(classes) {
     for (let classe of classes) {
         if (pontuacao[jogador][classe] === 3) {
             vitorias[jogador]++;
-            ultimoVencedor = jogador; // Define quem venceu por último
+            ultimoVencedor = jogador;
 
             atualizarPlacar();
             document.getElementById('vencedor').innerText = "Jogador " + jogador + " venceu!!";
-
-            setTimeout(function() {
-                alert("Jogador " + jogador + " venceu!!");
-            }, 100); 
-
             removerEventosDosCampos();
+
+            // ✅ SWEETALERT2 VITÓRIA
+            Swal.fire({
+                title: 'Vitória!',
+                text: 'Jogador ' + jogador + ' venceu!',
+                icon: 'success',
+                confirmButtonText: 'Jogar novamente'
+            }).then(() => {
+                iniciarJogo();
+            });
+
             return true;
         }
     }
@@ -100,14 +233,18 @@ function conferirResultado(classes) {
 
 function verificarEmpate() {
     if (jogadas >= 9) {
-        alert("Deu velha!!");
-        document.getElementById('vencedor').innerText = "Deu velha!!";
-
         removerEventosDosCampos();
+        document.getElementById('vencedor').innerText = "Deu velha!! Último a jogar: " + jogador;
 
-        // Se quiser alternar o jogador no empate, descomente as 2 linhas abaixo:
-        // trocarJogador();
-        // ultimoVencedor = jogador;
+        // ✅ SWEETALERT2 EMPATE
+        Swal.fire({
+            title: 'Empate!',
+            text: 'Deu velha!',
+            icon: 'info',
+            confirmButtonText: 'Jogar novamente'
+        }).then(() => {
+            iniciarJogo();
+        });
 
         return true;
     }
